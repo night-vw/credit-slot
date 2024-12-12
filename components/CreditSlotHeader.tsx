2af5a75bbede80ef1025031a8f36edd710ef280a
night-vw <waw78075@gmail.com>
2024-12-12 16:12:49 +0900

    fix:CreditSlotHeader

--- components/CreditSlotHeader.tsx
+++ components/CreditSlotHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CreditSlotHeader: React.FC = () => {
   return (
     <header className="bg-teal-600 text-white p-4 shadow-md fixed top-0 left-0 w-full z-50">
-      <h1 className="text-4xl md:text-4xl font-bold text-center">収支・ざ・ろっく！</h1>
+      <h1 className="text-3xl md:text-4xl font-bold text-center">収支・ざ・ろっく！</h1>
     </header>
   );
 };
